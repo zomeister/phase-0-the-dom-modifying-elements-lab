29b1a4229a5fc0d4a1d14dd685de896a359f0115
Zoe Guidry <oyezoey@gmail.com>
2022-10-14 04:11:18 -0500

    commit tree 7d227bdd5f02d946441fb910a5f0f7d24791a814

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.querySelector("main#main").remove();
+const newHeader = document.createElement('h1');
+newHeader.innerHTML = "<h1>ZOE is the champion</h1>";
+newHeader.id = "victory";
