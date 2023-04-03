ec4e0599a632878934f03500b05cf1c17b4f73d4
Zoe Guidry <oyezoey@gmail.com>
2023-04-03 08:54:36 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.getElementById('main')
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = "Zoe is the champion"
+document.body.appendChild(newHeader)
+
